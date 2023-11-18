530f7b845f52a2752db7d8dfb02e13ccd1eef82a
Николай <borisov8353@gmail.com>
2023-11-18 16:40:01 +0300

    update

--- Rpi.py
+++ Rpi.py
@@ -3,10 +3,13 @@ import socket
 from clover import srv
 from std_srvs.srv import Trigger
 import pretty_midi
-import matplotlib.pyplot as plt
 import time
+from clover.srv import SetLEDEffect
 
 rospy.init_node('flight_music')
+
+pretty_midi.note_number_to_name
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = ('0.0.0.0', 9999)
@@ -14,6 +17,25 @@ s.bind(server_address)
 
 midi_file_path = 'We Will Rock You.mid'
 midi_data = pretty_midi.PrettyMIDI(midi_file_path)
+
+instrument = midi_data.instruments[0]
+
+led_color = {
+    'A': {"r": 255, "g": 0, "b": 0},
+    'F': {"r": 0, "g": 255, "b": 0},
+    'G': {"r": 0, "g": 0, "b": 255},
+    'B': {"r": 148, "g": 0, "b": 211},
+    'C': {"r": 255, "g": 255, "b": 0},
+    'E': {"r": 66, "g": 170, "b": 255},
+    'D': {"r": 255, "g": 255, "b": 255},
+} 
+
+phases = [
+    [True, True],
+    [True, False],
+    [False, True],
+    [False, False]
+]
 
 get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
 navigate = rospy.ServiceProxy('navigate', srv.Navigate)
@@ -26,16 +48,30 @@ set_velocity = rospy.ServiceProxy('set_velocity', srv.SetVelocity)
 set_attitude = rospy.ServiceProxy('set_attitude', srv.SetAttitude)
 set_rates = rospy.ServiceProxy('set_rates', srv.SetRates)
 land = rospy.ServiceProxy('land', Trigger)
+set_effect = rospy.ServiceProxy('led/set_effect', SetLEDEffect)
 
-def dvig(kk = False):
-    if kk == False:
-        set_velocity(vx=1, vy=0.0, vz=0, frame_id='body')
-        rospy.sleep(0.2)
-        set_velocity(vx=0.0, vy=0.0, vz=0, frame_id='body')
-    elif kk == True:
-        set_velocity(vx=-1, vy=0.0, vz=0, frame_id='body')
-        rospy.sleep(0.2)
-        set_velocity(vx=0.0, vy=0.0, vz=0, frame_id='body')
+def dvig(phase):
+    if phase == [True, True]:
+        set_position(x=2, y=1, z=1, frame_id='aruco_map')
+    elif phase == [True, False]:
+        set_position(x=0, y=1, z=1, frame_id='aruco_map')
+    elif phase == [False, True]:
+        set_position(x=1, y=2, z=1, frame_id='aruco_map')
+    elif phase == [False, False]:
+        set_position(x=1, y=0, z=1, frame_id='aruco_map')
+
+
+    # for note in instrument.notes:
+    #     note_name = pretty_midi.note_number_to_name(note.pitch)
+    #     note_letter = note_name[0]
+    #     if note_letter == 'A': set_effect(r=255, g=0, b=0)
+    #     if note_letter == 'F': set_effect(r=0, g=255, b=0)  
+    #     if note_letter == 'G': set_effect(r=0, g=0, b=255)
+    #     if note_letter == 'B': set_effect(r=148, g=0, b=211)
+    #     if note_letter == 'C': set_effect(r=255, g=255, b=0)
+    #     if note_letter == 'E': set_effect(r=66, g=170, b=255)
+    #     if note_letter == 'D': set_effect(r=255, g=255, b=255) 
+
 
 navigate(x=0, y=0, z=1, frame_id='body', speed=0.5, auto_arm=True)  # взлет на 1 метра
 rospy.sleep(7)
@@ -47,14 +83,12 @@ while True:
     if data.decode('utf-8') == "Start": 
         break
 
-# rospy.sleep(5)
 
 a = midi_data.get_tempo_changes()[1][0]
 times = 1/(a/60)
 
-b = True
-
+rate = rospy.Rate(a/60)
 while True:
-    b = not b
-    dvig(b)
-    rospy.sleep(times-0.2)
+    for i in phases:
+        dvig(i)
+        rate.sleep()
